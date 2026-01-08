ed67d03af24e195e81a7cbbac4f3ce243d58647f
dhavalvaghela-commits <dhaval.vaghela@hiddenbrains.in>
2026-01-08 12:06:40 +0530

    Add Sphinx configuration file

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+# Configuration file for the Sphinx documentation builder.
+project = 'AI Development Guide'
+copyright = '2025, Hidden Brains'
+author = 'Hidden Brains'
+release = '1.0'
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'alabaster'
+html_static_path = ['_static']
